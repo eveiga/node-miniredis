8b13c20438eca0d1647059cc746d0cf3d7a327db
Edgar Veiga <edgarmveiga@gmail.com>
2013-03-14 18:18:54 +0000

    Proper indentation.

--- spec/execute-spec.js
+++ spec/execute-spec.js
@@ -6,21 +6,21 @@ require('mocha');
 var client = new MiniRedis("127.0.0.1", 6379);
 
 beforeEach(function () {
-	return client.flushdb();
+  return client.flushdb();
 });
 
 describe('get', function () {
-	describe('when a key exists', function () {
-		var key = 'key';
-		var expectedData = 'value';
-		beforeEach(function () {
-			return client.set(key, expectedData);
-		});
+  describe('when a key exists', function () {
+    var key = 'key';
+    var expectedData = 'value';
+    beforeEach(function () {
+      return client.set(key, expectedData);
+    });
 
-		it('returns its value', function () {
-			return client.get(key, function(data){
-				assert.strictEqual(data, expectedData);
-			});
-		});
-	});
+    it('returns its value', function () {
+      return client.get(key, function(data){
+        assert.strictEqual(data, expectedData);
+      });
+    });
+  });
 });
